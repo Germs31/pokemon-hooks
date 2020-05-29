8c92a4adce01ed7526f0ef06a158234cf5d7bb61
German Nunez <german90nunez@gmail.com>
2020-05-28 21:48:01 -0700

    get wild pokemon with axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
 import './index.css';
-import { directive } from '@babel/types';
 
 function App () {
+  const [pokedex, setPokedex] = useState([])
+  const [wildPokemon, setWildPokemon] = useState({});
+
+  useEffect(() => {
+    encounterWildPokemon()
+  }, [])
+
+  const encounterWildPokemon = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/151')
+      .then(res => {
+        console.log(res.data)
+        setWildPokemon(res.data)
+      })
+  }
+
   return(
     <div className="app-wrapper">
       hello, world
